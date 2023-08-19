54603c8c400530e82ca23dd5d951b894885544b5
choi ha young <gkdudk15@naver.com>
2023-08-19 22:21:23 +0900

    fix:logger.middleware 임포트 누락 수정

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,10 +1,10 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(req);
+    console.log(req.ip);
 
     next();
   }
