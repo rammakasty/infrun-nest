6d720b977c9e6fbf05d6a62fbe1f51c2dcd99924
choi ha young <gkdudk15@naver.com>
2023-08-20 14:11:10 +0900

    feat:예외처리 Get요청 성공

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,20 @@
-import { Controller } from '@nestjs/common';
-import { Request } from "express";
-import { Response } from '@nestjs/common';
-import { AppService } from 'src/app.service';
+import { Controller, HttpException, Patch } from '@nestjs/common';
+import { Delete, Get, Post, Put } from '@nestjs/common';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
-    constructor(private readonly catsService: CatsService) {
-        
+    constructor(private readonly catsService: CatsService) {}
+
+    @Get()
+    getAllCat() {
+        throw new HttpException('api is broken', 401)
+        return 'get all cat api'
+    }
+    @Post()
+    GetOneCat() {
         
     }
-}
+
+    }
+
