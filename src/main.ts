b1cb4d836c11944f3c6ad6ed2e3242cc08324ffe
choi ha young <gkdudk15@naver.com>
2023-09-26 02:45:59 +0900

    feat:main.ts Validation 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { HttpExceptionFilter } from 'common/exceptions/http-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe)
   app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT;
   await app.listen(PORT);
