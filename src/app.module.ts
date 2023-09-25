94fdb758d5f27fd66c314ebb5867f6daab886f1f
choi ha young <gkdudk15@naver.com>
2023-09-25 21:41:32 +0900

    feat:몽고디비 연결 성공!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,26 @@
-import { CatsService } from './cats/cats.service';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { UsersModule } from './users/users.module';
-import { LoggerMiddleware } from '../common/logger/logger.middleware';
+import { LoggerMiddleware } from 'common/logger/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    CatsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, CatsService],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev);
   }
 }
-
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { HttpExceptionFilter } from 'common/exceptions/http-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  const PORT = process.env.PORT;
+  await app.listen(PORT);
 }
 bootstrap();
