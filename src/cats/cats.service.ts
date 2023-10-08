809efb448193723a1f88cbdc39669f7345e54a48
choi ha young <gkdudk15@naver.com>
2023-10-08 12:08:29 +0900

    feat:bcrypt 추가

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,20 +1,22 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
-import { CatRequestDto } from './dto/CatRequestDto';
+import { CatRequestDto } from './dto/CatRequestDto'; // Assuming you have a DTO for incoming cat data
+import * as bcrypt from 'bcrypt'; // Import bcrypt library
 
 @Injectable()
 export class CatsService {
-    constructor(@Injectable(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
 
-    async signUp(body: CatsService) {
-        const { email, name, password} = body;
-        const isCatExist = await this.catModel.exists( {email} )
-
-        if (isCatExist){
-            throw new HttpException('해당하는 고양이는 이미 존재합니다.', 403)
-        }
-
+  async signUp(catRequestDto: CatRequestDto) {
+    const { email, name, password } = catRequestDto;
+    const isCatExist = await this.catModel.exists({ email });
+  
+    if (isCatExist) {
+        throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.')
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+}
 }
