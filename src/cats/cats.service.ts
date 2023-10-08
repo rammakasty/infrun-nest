d8553817579919c9c96f47054930deefbf513134
choi ha young <gkdudk15@naver.com>
2023-10-08 10:59:07 +0900

    feat:injection 주입 세팅

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/CatRequestDto';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+    constructor(@Injectable(Cat.name) private readonly catModel: Model<Cat>) {}
+
+    signUp(body: CatsService) {
+        
+    }
+}
