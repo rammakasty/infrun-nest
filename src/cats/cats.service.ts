8073943fafeb1f1f7fce29ff3681a8ddbabd14a1
choi ha young <gkdudk15@naver.com>
2023-10-08 11:23:39 +0900

    feat:고양이가 이미 존재할 때 예외처리

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
@@ -8,7 +8,13 @@ import { CatRequestDto } from './dto/CatRequestDto';
 export class CatsService {
     constructor(@Injectable(Cat.name) private readonly catModel: Model<Cat>) {}
 
-    signUp(body: CatsService) {
-        
+    async signUp(body: CatsService) {
+        const { email, name, password} = body;
+        const isCatExist = await this.catModel.exists( {email} )
+
+        if (isCatExist){
+            throw new HttpException('해당하는 고양이는 이미 존재합니다.', 403)
+        }
+
     }
 }
