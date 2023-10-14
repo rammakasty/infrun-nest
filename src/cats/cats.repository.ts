b5b3714456dcad8a9340e9ae471a43f19acbb56e
choi ha young <gkdudk15@naver.com>
2023-10-14 12:03:37 +0900

    fix: 타입스크립트 오류 해결

--- src/cats/cats.repository.ts
+++ src/cats/cats.repository.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
-import { CatRequestDto } from './dto/cats.request.dto';
+import { CatRequestDto } from './dto/CatRequestDto';
 
 @Injectable()
 export class CatsRepository {
@@ -11,7 +11,7 @@ export class CatsRepository {
 
   async existsByEmail(email: string): Promise<boolean> {
     const result = await this.catModel.exists({ email });
-    return result;
+    return !!result; // Use double negation to ensure the result is a boolean
   }
 
   async create(cat: CatRequestDto): Promise<Cat> {
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,30 +1,30 @@
-import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 import { Cat } from './cats.schema';
-import { CatRequestDto } from './dto/CatRequestDto'; // Assuming you have a DTO for incoming cat data
-import * as bcrypt from 'bcrypt'; // Import bcrypt library
+import { CatRequestDto } from './dto/CatRequestDto';
+import { CatsRepository } from './cats.repository';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
-  async signUp(catRequestDto: CatRequestDto) {
-    const { email, name, password } = catRequestDto;
-    const isCatExist = await this.catModel.exists({ email });
-  
+  async signUp(body: CatRequestDto) {
+    const { email, name, password } = body;
+    const isCatExist = await this.catsRepository.existsByEmail(email);
     if (isCatExist) {
-        throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.')
+      throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat = await this.catModel.create({
-        email,
-        name,
-        password: hashedPassword,
-    })
+    const cat = await this.catsRepository.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
 
-    return cat.readOnlyData
-}
+    return cat.readOnlyData;
+  }
 }
